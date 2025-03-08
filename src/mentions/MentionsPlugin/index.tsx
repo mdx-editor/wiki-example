6b5fe40d9aae211e95e82680cbd3aec4d9707718
Petyo Ivanov <underlog@gmail.com>
2025-03-08 08:54:48 +0200

    Upgrade to latest editor

--- src/mentions/MentionsPlugin/index.tsx
+++ src/mentions/MentionsPlugin/index.tsx
@@ -9,7 +9,7 @@
 import type { JSX } from 'react';
 import './style.css';
 
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext.js';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import {
   LexicalTypeaheadMenuPlugin,
   MenuOption,
